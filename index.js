5f6c5d1398926fdb0b26cd9ab60d5b2d64ad1573
srhee34 <60160057+joshrhee@users.noreply.github.com>
2023-09-18 00:37:07 -0400

    Make it as an website for demo purpose

---
+++ index.js
@@ -0,0 +1,35 @@
+document
+    .getElementById("wave-animation-button")
+    .addEventListener("click", () => {
+        window.location.href = "/Wave-Animation/index.html";
+    });
+
+document
+    .getElementById("grdation-animation-button")
+    .addEventListener("click", () => {
+        window.location.href = "/Gradation-Animation/index.html";
+    });
+
+document
+    .getElementById("bouncing-effect-animation-button")
+    .addEventListener("click", () => {
+        window.location.href = "/Bouncing-Effect-Animation/index.html";
+    });
+
+document
+    .getElementById("fireworks-animation-button")
+    .addEventListener("click", () => {
+        window.location.href = "/Fireworks-Animation/index.html";
+    });
+
+document
+    .getElementById("particle-animation-button")
+    .addEventListener("click", () => {
+        window.location.href = "/Particle-Practice/index.html";
+    });
+
+document
+    .getElementById("kinetic-typography1-button")
+    .addEventListener("click", () => {
+        window.location.href = "/Kinetic Typography1/index.html";
+    });
